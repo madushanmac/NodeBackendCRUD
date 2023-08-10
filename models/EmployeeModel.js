69665cf527007d577372ad4408e9798ff19e491a
madushanmac <Chamindumadushan1996org@gmail.com>
2023-08-10 09:04:17 +0530

    node CRUD Employee
    node CRUD Employee

---
+++ models/EmployeeModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const employeeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    nic: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("employee", employeeSchema);
